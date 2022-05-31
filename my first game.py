5811d430ffc86683b1abeac664ad80c4069d633d
69Lucky6912 <103605661+69Lucky6912@users.noreply.github.com>
2022-05-31 16:33:24 +0100

    Add files via upload

---
+++ my first game.py
@@ -0,0 +1,9 @@
+#trying to make a hangmen game
+game  = input("what word do u choose: ")
+game  = game.lower
+cheak = input("the word that you have use is :{game} ")
+#every time there is a error
+gtest = game[4]
+print(g)
+
+
